a812731b13f15cdca5e0461cfab93f5c1e636431
Pratyush Chandra <pratyushchandra@Pratyushs-MacBook-Air.local>
2023-06-02 19:19:23 +0530

    dfdsf

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,3 +6,5 @@ const About = () =>{
 };
 
 export default About;
+// CONNECTION_STRING=mongodb+srv://bhaveshchemicool:e4emoqA6KvjlARaC@profilecluster.zjex6nk.mongodb.net/Contact-me?retryWrites=true&w=majority
+// PORT=8000
